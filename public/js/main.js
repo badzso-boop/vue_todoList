0ac3e0d46048f956e69b4af49bd2c3d7abff5953
Badzso <norbert.ujj@gmail.com>
2021-03-18 19:31:45 +0100

    files

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,3 @@
-Vue.component('navigacio', {
-    template: '<nav class="navbar navbar-expand-xl bg-dark navbar-dark">\
-    <a class="navbar-brand" href="#">Családi Napló</a>\
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">\
-        <span class="navbar-toggler-icon"></span>\
-    </button>\
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">\
-        <ul class="navbar-nav">\
-            <li class="nav-item">\
-                <a class="nav-link py-2 px-3" href="">Belépés/Regisztráció</a>\
-            </li>\
-            <li class="nav-item">\
-                <a class="nav-link py-2 px-3" href="main.html">Főoldal</a>\
-            </li>\
-            <li class="nav-item">\
-                <a class="nav-link py-2 px-3" href="teendok.html">Teendők</a>\
-            </li>\
-            <li class="nav-item">\
-                <a class="nav-link py-2 px-3" href="">Géphasználat</a>\
-            </li>\
-            <li class="nav-item">\
-                <a class="nav-link py-2 px-3" href="">Kilépés</a>\
-            </li>\
-            <li class="nav-item">\
-                <a class="nav-link py-2 px-3" href="">Felhasználói profil</a>\
-            </li>\
-        </ul>\
-    </div>\
-</nav>'
-})
-
-Vue.component('lab',{
-    template: '<footer class="bg-dark text-center text-white fixed-bottom">\
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">\
-        © 2021 \
-        <a class="text-white font-weight-bold" href="#">Programozó Palánták</a>\
-    </div>\
-</footer>'
-})
-
 var app2 = new Vue({
     el: '#oldal',
     data: {
@@ -78,7 +38,6 @@ var app2 = new Vue({
                 this.errormsgreg = 'A jelszavak nem egyeznek meg!'
             } else {
                 this.errormsgreg = 'Sikeres Regisztráció!'
-                console.log(this.reg)
                 axios({
                     method: 'post',
                     url: '/login',
@@ -87,6 +46,9 @@ var app2 = new Vue({
                     }
                 });
             }
+        },
+        belepes: function() {
+
         },
         kitoltes: function() {
             this.reg.nev = 'Ujj Norbert',
